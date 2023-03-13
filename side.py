defd92ea1c4e3b41c99015105980ea396ed3650a
Thrilok <92184137+Thrilok05@users.noreply.github.com>
2023-03-13 13:41:22 +0530

    Add files via upload

--- side.py
+++ side.py
@@ -1,18 +1,32 @@
 from flask import Flask, request, render_template
+import pymysql as pql
+
 
 app = Flask(__name__)
+connection = pql.connect(host='localhost',user='root',password='',database='project',)
 
-@app.route("/", methods=["POST","GET"])
-def main():
-    return render_template('sidemain.html')
+cur=connection.cursor()
+cur.execute("CREATE TABLE IF NOT EXISTS data (id INT NOT NULL AUTO_INCREMENT,name varchar(16) NOT NULL,Email varchar(30) NOT NULL,PRIMARY KEY (id))")
+connection.commit()
+
 
 @app.route("/", methods=["POST"])
-def form():
-     service=request.form["service"]
-     name=request.form.get["name"]
-     return render_template('sidemain.html', service=service,name=name)
+def main():
+    
+    return render_template('sidemain.html')
 
-
+@app.route("/home",methods=["post","get"])
+def remain():
+    if request.method=="post":
+        name=request.form["name"]
+        email=request.form["email"]
+        with connection:
+            with connection.cursor() as cursor:
+                qry="INSERT INTO data('name','E-mail') values(%s,%s)"
+                cursor.execute(qry,(name,email))
+            connection.commit()
+          
+    return f"name: {name}\nemail: {email}"
 
 if __name__ == '__main__':
     app.run(debug=True)
